caea6908aafb1400c33bdcf3256bae29ea16a630
marcelo <marc.bianchi@gmail.com>
2021-10-10 10:29:55 -0300

    added groups and change main view

---
+++ KleverTest/view/UserAccountView.swift
@@ -0,0 +1,20 @@
+//
+//  UserAccountView.swift
+//  KleverTest
+//
+//  Created by marcelo bianchi on 10/10/21.
+//
+
+import SwiftUI
+
+struct UserAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserAccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserAccountView()
+    }
+}
