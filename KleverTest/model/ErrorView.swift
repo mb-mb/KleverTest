09d2faa9e44d9a51b900e54027e6067985246039
marcelo <marc.bianchi@gmail.com>
2021-10-10 21:36:29 -0300

    view with mock list

---
+++ KleverTest/model/ErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  ErroView.swift
+//  KleverTest
+//
+//  Created by marcelo bianchi on 10/10/21.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text("Ocorreu um erro no aplicativo, favor verificar com suporte")
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}
--- KleverTest/view/UserAccountContentView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserAccountContentView.swift
-//  KleverTest
-//
-//  Created by marcelo bianchi on 10/10/21.
-//
-
-import Foundation
